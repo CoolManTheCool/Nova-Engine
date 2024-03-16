70c606650b5724b57694076420f57931779d96cd
CoolManTheCool <noahgrimes0915@gmail.com>
2024-03-16 12:17:46 -0500

    vulkan stuff

---
+++ build/CMakeFiles/Dynamic-Voxel-Craft.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dynamic-Voxel-Craft.dir/src/app/app.cpp.o"
+  "CMakeFiles/Dynamic-Voxel-Craft.dir/src/app/app.cpp.o.d"
+  "CMakeFiles/Dynamic-Voxel-Craft.dir/src/device/device.cpp.o"
+  "CMakeFiles/Dynamic-Voxel-Craft.dir/src/device/device.cpp.o.d"
+  "CMakeFiles/Dynamic-Voxel-Craft.dir/src/main.cpp.o"
+  "CMakeFiles/Dynamic-Voxel-Craft.dir/src/main.cpp.o.d"
+  "CMakeFiles/Dynamic-Voxel-Craft.dir/src/pipeline/pipeline.cpp.o"
+  "CMakeFiles/Dynamic-Voxel-Craft.dir/src/pipeline/pipeline.cpp.o.d"
+  "CMakeFiles/Dynamic-Voxel-Craft.dir/src/window/window.cpp.o"
+  "CMakeFiles/Dynamic-Voxel-Craft.dir/src/window/window.cpp.o.d"
+  "Dynamic-Voxel-Craft"
+  "Dynamic-Voxel-Craft.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dynamic-Voxel-Craft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
